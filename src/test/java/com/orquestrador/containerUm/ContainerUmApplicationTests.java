1bd870829e366295db0f6f29aadbd6790b537c31
randolfdev <117084719+randolfdev@users.noreply.github.com>
2022-10-31 23:06:45 -0300

    Strutura Back-end Containers

---
+++ src/test/java/com/orquestrador/containerUm/ContainerUmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orquestrador.containerUm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContainerUmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
